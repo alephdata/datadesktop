3782d8ddc9bc6cf6d8cf5fcde310bc083f210b88
kjacks <kjack227@gmail.com>
2019-10-01 13:19:01 +0200

    added notarize script to scripts folder

---
+++ scripts/notarize.js
@@ -0,0 +1,38 @@
+// See: https://medium.com/@TwitterArchiveEraser/notarize-electron-apps-7a5f988406db
+
+require('dotenv').config();
+const fs = require('fs');
+const path = require('path');
+var electron_notarize = require('electron-notarize');
+
+module.exports = async function (params) {
+    // Only notarize the app on Mac OS only.
+    if (params.electronPlatformName !== 'darwin') {
+        return;
+    }
+
+    console.log('afterSign hook triggered');
+
+    // Same appId in electron-builder.
+    let appId = 'com.electron.visdesktop'
+
+    let appPath = path.join(params.appOutDir, `${params.packager.appInfo.productFilename}.app`);
+    if (!fs.existsSync(appPath)) {
+        throw new Error(`Cannot find application at: ${appPath}`);
+    }
+
+    console.log(`Notarizing ${appId} found at ${appPath}`);
+
+    try {
+        await electron_notarize.notarize({
+            appBundleId: appId,
+            appPath: appPath,
+            appleId: process.env.APPLE_ID,
+            appleIdPassword: process.env.APPLE_APP_PASSWORD,
+        });
+    } catch (error) {
+        console.error(error);
+    }
+
+    console.log(`Done notarizing ${appId}`);
+};
