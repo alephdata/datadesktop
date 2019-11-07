fe153d9823eae35e03dd92d6b6a797806d1362c6
kjacks <kjack227@gmail.com>
2019-11-07 11:59:12 +0100

    upgraded dependencies

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -6,6 +6,7 @@ import { defaultModel, Model} from '@alephdata/followthemoney'
 import '@blueprintjs/core/lib/css/blueprint.css';
 import '@blueprintjs/select/lib/css/blueprint-select.css'
 import '@blueprintjs/datetime/lib/css/blueprint-datetime.css'
+import '@blueprintjs/table/lib/css/table.css';
 import './App.css';
 
 FocusStyleManager.onlyShowFocusOnTabs();
